1fbce671fedf9d0b8dcd50ca7461cf195c03b9a3
sundong <sundong@yingu.com>
2017-01-19 15:01:41 +0800

    统一gradle版本

--- demo/src/main/java/com/lewish/start/demo/view/RobotWaveView.java
+++ demo/src/main/java/com/lewish/start/demo/view/RobotWaveView.java
@@ -14,10 +14,10 @@ import android.view.View;
 
 import com.lewish.start.demo.R;
 
-/**
+/** 
  * author: sundong
- * created at 2017/1/18 11:33
- */
+ * created at 2017/1/19 14:55
+ */  
 public class RobotWaveView extends View {
 
     // Xfermode
@@ -25,19 +25,19 @@ public class RobotWaveView extends View {
     // 画笔
     private Paint paint;
     // 源图片
-    private Bitmap bitmap;
+    private Bitmap mSrcBitmap;
     // 控件宽高
     private int width, height;
     // 画贝塞尔曲线Path
-    private Path path;
+    private Path mBezierPath;
     // 在该画布上绘制目标图片
     private Canvas mCanvas;
     // 目标图片
-    private Bitmap bg;
+    private Bitmap mDstBitmap;
     // 贝塞尔曲线控制点，使用三阶贝塞尔曲线曲线，需要两个控制点，两个控制点都在该变量基础上生成
     private float controlX, controlY;
     // 上升的高度
-    private float waveY;
+    private float mWaveHeight;
     // 用于控制控制点水平移动
     private boolean isIncrease;
 
@@ -77,23 +77,23 @@ public class RobotWaveView extends View {
         paint.setStyle(Paint.Style.FILL);
         paint.setColor(Color.parseColor("#ffc9394a"));
         // 获得资源文件
-        bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+        mSrcBitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
         // 设置宽高为图片的宽高
-        width = bitmap.getWidth();
-        height = bitmap.getHeight();
+        width = mSrcBitmap.getWidth();
+        height = mSrcBitmap.getHeight();
         // 初始状态值
-        waveY = 7 / 8F * height;
+        mWaveHeight = 7 / 8F * height;
         controlY = 17 / 16F * height;
         // 初始化Xfermode
         porterDuffXfermode = new PorterDuffXfermode(PorterDuff.Mode.SRC_IN);
         // 初始化path
-        path = new Path();
+        mBezierPath = new Path();
         // 初始化画布
         mCanvas = new Canvas();
         // 创建bitmap
-        bg = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        mDstBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
         // 将新建的bitmap注入画布
-        mCanvas.setBitmap(bg);
+        mCanvas.setBitmap(mDstBitmap);
     }
 
     @Override
@@ -101,15 +101,15 @@ public class RobotWaveView extends View {
         // 画目标图，存在bg上
         drawTargetBitmap();
         // 将目标图绘制在当前画布上，起点为左边距，上边距的交点
-        canvas.drawBitmap(bg, getPaddingLeft(), getPaddingTop(), null);
+        canvas.drawBitmap(mDstBitmap, getPaddingLeft(), getPaddingTop(), null);
         invalidate();
     }
 
     private void drawTargetBitmap() {
         // 重置path
-        path.reset();
+        mBezierPath.reset();
         // 擦除像素
-        bg.eraseColor(Color.parseColor("#00ffffff"));
+        mDstBitmap.eraseColor(Color.parseColor("#00ffffff"));
         // 当控制点的x坐标大于或等于终点x坐标时更改标识值
         if (controlX >= width + 1 / 2 * width) {
             isIncrease = false;
@@ -123,28 +123,28 @@ public class RobotWaveView extends View {
         if (controlY >= 0) {
             // 波浪上移
             controlY -= 1;
-            waveY -= 1;
+            mWaveHeight -= 1;
         } else {
             // 超出则重置位置
-            waveY = 7 / 8F * height;
+            mWaveHeight = 7 / 8F * height;
             controlY = 17 / 16F * height;
         }
         // 贝塞尔曲线的生成
-        path.moveTo(0, waveY);
+        mBezierPath.moveTo(0, mWaveHeight);
         // 两个控制点通过controlX，controlY生成
-        path.cubicTo(controlX / 2, waveY - (controlY - waveY),
-                (controlX + width) / 2, controlY, width, waveY);
+        mBezierPath.cubicTo(controlX / 2, mWaveHeight - (controlY - mWaveHeight),
+                (controlX + width) / 2, controlY, width, mWaveHeight);
         // 与下下边界闭合
-        path.lineTo(width, height);
-        path.lineTo(0, height);
+        mBezierPath.lineTo(width, height);
+        mBezierPath.lineTo(0, height);
         // 进行闭合
-        path.close();
+        mBezierPath.close();
         // 画慕课网logo
-        mCanvas.drawBitmap(bitmap, 0, 0, paint);
+        mCanvas.drawBitmap(mSrcBitmap, 0, 0, paint);
         // 设置Xfermode
         paint.setXfermode(porterDuffXfermode);
         // 画三阶贝塞尔曲线
-        mCanvas.drawPath(path, paint);
+        mCanvas.drawPath(mBezierPath, paint);
         // 重置Xfermode
         paint.setXfermode(null);
     }
